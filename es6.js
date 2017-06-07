7b921b2d906e828f5f122774cb6a88ec16e8ad55
Ariyan Khan <ariyankhan46@gmail.com>
2017-06-08 02:24:49 +0530

    add support for symbol

--- es6.js
+++ es6.js
@@ -3,12 +3,11 @@
  * This module provides an approximate equivalent implementation of ES6(Harmony)'s
  * new features in pure ES5 for backward compatibility. This API has no dependencies.
  *
- * Warning: This implementation is basically on firefox browser.
+ * Warning: This implementation is tested only on firefox browser.
  *
  * Codebase: https://github.com/ariyankhan/es6
  *
  */
-
 
 
 (function (global, factory) {
@@ -20,22 +19,30 @@
          * For the environment like NodeJS, CommonJS etc where module or
          * module.exports objects are available to export ES6 object.
          */
-        module.exports = factory();
+        module.exports = factory(global);
     } else {
         /*
          * For browser environment, where global refers to the window
          * object and the ES6 API is exported as 'ES6' property in window object.
          */
-        global["ES6"] = factory();
+        global["ES6"] = factory(global);
     }
 
-})(this, function () {
+    //window is for browser context and global is for NodeJS context
+})(typeof window !== "undefined" ? window : global, function (global) {
 
     "use strict";
 
     var ES6 = {};
 
 
+    function generateSymbolName(desc) {
+        desc = validateSymbolDesc(desc);
+
+    }
+    function validateSymbolDesc(desc) {
+        return typeof desc === "undefined" ? "" : String(desc);
+    }
 
     return ES6;
 });
