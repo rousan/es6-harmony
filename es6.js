f7905ea56b4fcc6d99db8c723687b759ed502f43
Ariyan Khan <ariyankhan46@gmail.com>
2017-06-12 01:51:51 +0530

    Update About

--- es6.js
+++ es6.js
@@ -1,8 +1,8 @@
 /*!
- * ES6 v1.0.0
+ * ES6 v1.0.1
  * This module provides an equivalent implementation of ES6(Harmony)
- * in pure ES5 code and creates a ES6 environment for backward browsers or JavaScript engines
- * that does not support ES6.(At least ES5 is required).
+ * in pure ES5 code and creates a ES6 environment for backward browsers or
+ * JavaScript engines that natively does not support ES6.(At least ES5 is required).
  * This Library is standalone, it has no dependency.
  *
  * @license Copyright (c) 2017 Ariyan Khan, MIT License
